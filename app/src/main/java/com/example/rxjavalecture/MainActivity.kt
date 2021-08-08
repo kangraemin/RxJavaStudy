5689cc3d00772d9319f48ff19b5b7e0448e66e80
kangraemin <remin1994@gmail.com>
2021-08-08 21:37:46 +0900

    [Init] Create Project

---
+++ app/src/main/java/com/example/rxjavalecture/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rxjavalecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.reactivex.Observable
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
