57227b6eba4cc4bde51c295de768883c50c69aff
kangraemin <remin1994@gmail.com>
2021-08-16 21:25:38 +0900

    [Add] Reactive programming example UI
    - Add rxBinding gradle dependency ( 3.1.0 )

--- AndroidProject/app/src/main/java/com/example/rxjavalecture/ExampleItem.kt
+++ AndroidProject/app/src/main/java/com/example/rxjavalecture/ExampleItem.kt
@@ -3,6 +3,7 @@ package com.example.rxjavalecture
 import android.app.Activity
 import androidx.annotation.StringRes
 import com.example.rxjavalecture.introduction.nonreactive.NonReactiveExampleActivity
+import com.example.rxjavalecture.introduction.reactive.ReactiveExampleActivity
 import com.example.rxjavalecture.observerpattern.noneobserverpattern.NoneObserverPatternActivity
 import com.example.rxjavalecture.observerpattern.oberserverpattern.ObserverPatternActivity
 
@@ -10,4 +11,5 @@ enum class ExampleItem(val destinationClass: Class<out Activity>, @StringRes val
     NONE_OBSERVER_PATTERN(NoneObserverPatternActivity::class.java, R.string.first_week_observer_pattern_example_first),
     OBSERVER_PATTERN(ObserverPatternActivity::class.java, R.string.first_week_observer_pattern_example_second),
     NONE_REACTIVE_EXAMPLE(NonReactiveExampleActivity::class.java, R.string.first_week_reactive_programming_example_first),
+    REACTIVE_EXAMPLE(ReactiveExampleActivity::class.java, R.string.first_week_reactive_programming_example_second)
 }
