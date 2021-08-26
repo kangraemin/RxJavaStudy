eb39b9a0278fc90e015a7b8e7686f5c69a8e5eca
Mingu Seo <tjalsrnek@gmail.com>
2021-08-26 17:43:31 +0900

    [Add] ObserverPattern Implementation

---
+++ Homework/Gooreum/ObserverPattern/ObserverPattern/CustomTextField.swift
@@ -0,0 +1,29 @@
+//
+//  PercentTextField.swift
+//  ObserverPattern
+//
+//  Created by Mingu Seo on 2021/08/22.
+//
+
+
+import UIKit
+
+class CustomTextField: ObserverProtocol {
+    var txtParency: UITextField!
+    
+    init(txtParency: UITextField) {
+        self.txtParency = txtParency
+        MainViewController.progressSubject.subscribe(observer: self)
+        self.txtParency.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    @objc func textFieldDidChange(_ sender: Any?) {
+        if let value = self.txtParency.text {
+            MainViewController.progressSubject.percentValue = value
+        }
+    }
+    
+    func notifyDataIsChanged<T>(value: T) {
+        self.txtParency.text = "\(value)"
+    }
+}
