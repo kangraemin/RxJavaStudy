cbfbad1ad23e9a3dca9d5aba3d110c6227a17590
kangraemin <remin1994@gmail.com>
2021-09-16 00:06:40 +0900

    [Add] Example code for subject / hot, cold observable

--- AndroidProject/app/src/main/java/com/example/rxjavalecture/ExampleItem.kt
+++ AndroidProject/app/src/main/java/com/example/rxjavalecture/ExampleItem.kt
@@ -8,6 +8,7 @@ import com.example.rxjavalecture.observerpattern.noneobserverpattern.NoneObserve
 import com.example.rxjavalecture.observerpattern.oberserverpattern.ObserverPatternActivity
 import com.example.rxjavalecture.scheduler.SchedulerExampleActivity
 import com.example.rxjavalecture.streamimplementation.StreamImplementationExampleActivity
+import com.example.rxjavalecture.subject.SubjectExampleActivity
 
 enum class ExampleItem(val destinationClass: Class<out Activity>, @StringRes val exampleTitleRes: Int) {
     NONE_OBSERVER_PATTERN(NoneObserverPatternActivity::class.java, R.string.first_week_observer_pattern_example_first),
@@ -15,5 +16,6 @@ enum class ExampleItem(val destinationClass: Class<out Activity>, @StringRes val
     NONE_REACTIVE_EXAMPLE(NonReactiveExampleActivity::class.java, R.string.first_week_reactive_programming_example_first),
     REACTIVE_EXAMPLE(ReactiveExampleActivity::class.java, R.string.first_week_reactive_programming_example_second),
     STREAM_IMPLEMENTATION_EXAMPLE(StreamImplementationExampleActivity::class.java, R.string.third_week_stream_implementation_example_title),
-    SCHEDULER_EXAMPLE(SchedulerExampleActivity::class.java, R.string.six_week_scheduler);
+    SCHEDULER_EXAMPLE(SchedulerExampleActivity::class.java, R.string.six_week_scheduler),
+    SUBJECT_EXAMPLE(SubjectExampleActivity::class.java, R.string.seven_week_example_title);
 }
