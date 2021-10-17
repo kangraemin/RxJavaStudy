847aac77e0e69840e3fc1ecb676f92b08e8870de
kangraemin <remin1994@gmail.com>
2021-10-17 21:49:03 +0900

    [Add] Example code for transform callback to custom stream

--- AndroidProject/app/src/main/java/com/example/rxjavalecture/exercise/data/repository/login/LoginRepositoryImpl.kt
+++ AndroidProject/app/src/main/java/com/example/rxjavalecture/exercise/data/repository/login/LoginRepositoryImpl.kt
@@ -1,11 +1,14 @@
 package com.example.rxjavalecture.exercise.data.repository.login
 
+import android.util.Log
 import com.example.rxjavalecture.exercise.data.local.token.LocalTokenDataSource
 import com.example.rxjavalecture.exercise.data.local.token.LocalTokenMapper.mappingRemoteDataToLocal
 import com.example.rxjavalecture.exercise.data.remote.base.BaseCallback
 import com.example.rxjavalecture.exercise.data.remote.login.LoginDataSource
 import com.example.rxjavalecture.exercise.data.remote.login.LoginItem
+import com.google.firebase.messaging.FirebaseMessaging
 import io.reactivex.Completable
+import io.reactivex.Single
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,6 +17,19 @@ class LoginRepositoryImpl(
     private val localTokenDataSource: LocalTokenDataSource,
     private val loginDataSource: LoginDataSource
 ) : LoginRepository {
+
+    private val getFirebaseTokenSingle: Single<String> by lazy {
+        Single.create { emitter ->
+            FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.d("FCM", "fail to get token = ${task.exception?.localizedMessage}")
+                    emitter.onError(task.exception!!)
+                }
+                Log.d("FCM", "success to get token = ${task.result}")
+                emitter.onSuccess(task.result!!)
+            }
+        }
+    }
 
     override fun loginRetrofitCall(id: String, password: String, callback: BaseCallback<Unit>) {
         loginDataSource
@@ -49,6 +65,7 @@ class LoginRepositoryImpl(
                 localTokenDataSource
                     .deleteAllCachedToken()
                     .andThen(localTokenDataSource.saveToken(mappingRemoteDataToLocal(it)))
+                    .andThen(getFirebaseTokenSingle.ignoreElement())
             }
     }
 }
