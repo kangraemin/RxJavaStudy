5afcd88e37e59afa8969c3c31308e7d6d582b4ec
handnew <handnew04@gmail.com>
2022-04-05 16:06:07 +0900

    feat: 패키지 정리

---
+++ Homework/Handnew04/RxStudy/app/src/main/java/com/example/rxstudy/observerpattern/ui/ObserverPatternActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rxstudy.observerpattern.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+class ObserverPatternActivity : AppCompatActivity() {
+}
---
+++ Homework/Handnew04/RxStudy/app/src/main/java/com/example/rxstudy/observerpattern/ui/PercentTextView.kt
@@ -0,0 +1,15 @@
+package com.example.rxstudy.observerpattern.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class PercentTextView : AppCompatTextView {
+   constructor(context: Context) : super(context)
+   constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+   constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+      context,
+      attrs,
+      defStyleAttr
+   )
+}
