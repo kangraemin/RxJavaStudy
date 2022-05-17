1908c54bbc05b4e7290a68eb2d661cf75edbabd7
kimriri <dmsal547@gmail.com>
2022-05-18 02:39:02 +0900

    4주차 과제

--- RxKotlin/app/src/main/java/com/example/rxkotlin/oberserverpattern/ObserverPatternActivity.kt
+++ RxKotlin/app/src/main/java/com/example/rxkotlin/oberserverpattern/ObserverPatternActivity.kt
@@ -1,12 +1,17 @@
 package com.example.rxjavalecture.observerpattern.oberserverpattern
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rxjavalecture.observerpattern.oberserverpattern.customui.PercentEditText
 import com.example.rxjavalecture.observerpattern.oberserverpattern.customui.PercentImageView
 import com.example.rxjavalecture.observerpattern.oberserverpattern.customui.PercentSeekBar
 import com.example.rxjavalecture.observerpattern.oberserverpattern.customui.PercentTextView
 import com.example.rxkotlin.R
+import com.example.rxkotlin.com.Week4Assignment
+import java.nio.channels.InterruptedByTimeoutException
 
 class ObserverPatternActivity : AppCompatActivity() {
 
@@ -14,6 +19,7 @@ class ObserverPatternActivity : AppCompatActivity() {
     private lateinit var sbPercent: PercentSeekBar
     private lateinit var tvPercent: PercentTextView
     private lateinit var imgOpacityResult: PercentImageView
+    private lateinit var week4Assignment_btn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +29,18 @@ class ObserverPatternActivity : AppCompatActivity() {
         sbPercent = findViewById(R.id.sb_percent)
         tvPercent = findViewById(R.id.tv_percent)
         imgOpacityResult = findViewById(R.id.img_opacity_result)
+        week4Assignment_btn = findViewById(R.id.Week4Assignment_btn)
+        week4Assignment_btn.setOnClickListener{goToweek4Assignment()}
+
+
     }
+
+    private fun goToweek4Assignment() {
+        val goToweek4AssignmentIntent : Intent = Intent(this,Week4Assignment::class.java)
+        startActivity(goToweek4AssignmentIntent)
+        finish()
+    }
+
 
     companion object {
         val progressSubject = RxKotlinSubject<Int>()
