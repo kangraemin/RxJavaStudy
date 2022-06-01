d0e4aaa74019a1e439a2bdab2581cbda5b891048
kimriri <dmsal547@gmail.com>
2022-06-01 21:08:47 +0900

    5주차 과제

--- Homework/kimriri/app/src/main/java/com/example/rxkotlin/oberserverpattern/Week5Assignment.kt
+++ Homework/kimriri/app/src/main/java/com/example/rxkotlin/oberserverpattern/Week5Assignment.kt
@@ -3,7 +3,8 @@ package com.example.rxkotlin.com
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rxkotlin.R
-import io.reactivex.Observable
+import io.reactivex.*
+import io.reactivex.Observable.create
 
 
 /** 각자 플랫폼에 맞게
@@ -18,26 +19,54 @@ class Week5Assignment : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_week_5_assignment)
         val observerJust= Observable.just(11, 12, 13)
-      val  observerJustReturn = observerJust.subscribe(
+            .subscribe(
+                { println("onNext $it") },
+                { println("onError") },
+                { println("onComplete") },
+                { println("onSubscribe") })
+
+        val observableCreate = Observable.create<String> {
+            it.onNext("onNext")
+            it.onError(error( "onError"))
+            it.onComplete()
+        }.subscribe (
             { println("onNext $it") },
             { println("onError") },
             { println("onComplete") },
             { println("onSubscribe") })
 
-        val xSubject = Observable.create<Int>()
-        xSubject.subscribe { println("첫번째 $it") }
-        xSubject.onNext(1)
-        Thread.sleep(1000L)
-        xSubject.subscribe { println("----두번째 $it") }
-        xSubject.onNext(2)
-        xSubject.onNext(3)
-        Thread.sleep(1000L)
-        xSubject.subscribe { println("********세번째 $it") }
-        xSubject.onNext(4)
-        xSubject.onComplete()
+        val SingleJust = Single
+            .just("SingleJust")
+            .subscribe(
+                { print("SingleJust") },
+                { it.printStackTrace() }
+            )
+
+        val SingleCreate = create<String> {
+            it.onNext("SingleCreate")
+        }.subscribe(
+                { print("SingleCreate") },
+                { it.printStackTrace() }
+            )}
+
+    val FlowableJust= Flowable.just(11, 12, 13)
+        .subscribe(
+            { println("onNext $it") },
+            { println("onError") },
+            { println("onComplete") },
+            { println("onSubscribe") })
 
 
+    val MaybeJust= Maybe
+        .just(11)
+        .subscribe(
+            { println("onNext $it") },
+            { println("onError") },
+            { println("onComplete") }
+        )
+
+    val CompletableJust= Completable.complete()
+        .subscribe { println("onComplete") }
 
 
-    }
 }
